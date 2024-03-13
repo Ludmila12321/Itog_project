65e2762f4cd407cf7d6b64651ee01a6bcc1a56c5
Lyudmila <lyudmila.kostornova@mail.ru>
2024-03-14 01:18:29 +0300

    Добавлена фильтрация в каталоге

--- Project_itog/catalogue_app/views.py
+++ Project_itog/catalogue_app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-
-from administration_app.models import Animal
+from administration_app.models import Animal, Category, Gender
 
 def animal_catalog(request):
-    animals = Animal.objects.all()  # Получение всех объектов Animal
+    categories = Category.objects.all()
+    genders = Gender.objects.all()
 
-    return render(request, 'animal_catalog.html', {'animals': animals})
+    selected_category = request.GET.get('category')
+    selected_gender = request.GET.get('gender')
+
+    animals = Animal.objects.all()
+
+    if selected_category:
+        animals = animals.filter(category__title=selected_category)
+
+    if selected_gender:
+        animals = animals.filter(gender__gender=selected_gender)
+
+    return render(request, 'animal_catalog.html', {'animals': animals, 'categories': categories, 'genders': genders, 'selected_category': selected_category, 'selected_gender': selected_gender})
